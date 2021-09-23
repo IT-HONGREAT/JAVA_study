e90593104b52ae6c65320b2e71873985832f6574
IT-HONGREAT <hongreat95@gmail.com>
2021-09-23 18:27:49 +0900

    변수를 지정함에 있어서 확실히 복잡하다

--- src/Variables.java
+++ src/Variables.java
@@ -11,11 +11,11 @@ public class Variables {
         //age += 1 ;   //이것도 가능하네!
 
 
-        System.out.print("Hello, i'm ");  //그냥 print는 줄바꿈이 안되고
-        System.out.println(age);          //println 은 줄바꿈이된다.!!
-        System.out.println("만 나이는 아마도");
-        System.out.print(man_age);
-        System.out.println("살??");
+//        System.out.print("Hello, i'm ");  //그냥 print는 줄바꿈이 안되고
+//        System.out.println(age);          //println 은 줄바꿈이된다.!!
+//        System.out.println("만 나이는 아마도");
+//        System.out.print(man_age);
+//        System.out.println("살??");
 
         //boolean 은 불린자료형이고
         // int는 정수이고 2^-(31)~2^(31)의 범위를 가진다,long는 int 범위 외의 큰 값이다.
@@ -29,9 +29,30 @@ public class Variables {
 
         double check_double = 3.141592;
 
-        System.out.println(check_bool);
-        System.out.println(check_int);
-        System.out.println(check_long);
-        System.out.println(check_double);
+//        System.out.println(check_bool);
+//        System.out.println(check_int);
+//        System.out.println(check_long);
+//        System.out.println(check_double);
+//
+//        자료형 연습
+        String name;
+        name = "홍인영";
+        int year = 2021;
+        int month = 9;
+        int day = 23;
+        boolean married;
+        married = false;
+        String p1;
+        p1 = "이름: ";
+        String p2;
+        p2 = "날짜: ";
+        String p3;
+        p3 = "결혼: ";
+
+        System.out.print(p1);
+        System.out.println(name);
+        System.out.println(p2+year+"년"+month+"월"+day+"일");
+        System.out.print(p3+married);
+
     }
 }
