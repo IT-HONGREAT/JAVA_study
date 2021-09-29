e0a7529ebc29ff5b39779fa16d7099318fae8534
IT-HONGREAT <hongreat95@gmail.com>
2021-09-29 22:56:55 +0900

    for 문; 파이썬과 비슷한 것 같다.

--- src/For.java
+++ src/For.java
@@ -17,5 +17,13 @@ public class For {
 //       for문의 구성요소는 (초기화식;종결제어식;증감제어식;) 이다.
 //         초기화식은 초기 시작점 세팅, 종결제어식은 범위세팅, 증감제어식은 step의 개념이다.
         System.out.println(check);
+
+//        1~100 합 더해보기
+        int hundred = 0;
+        for (int i = 1; i<= 100; i++){
+            hundred += i;
+        }
+        System.out.println("1~100 합 : "+hundred);
+//        신기한점: 문자열에서 '' 는 에러가 발생하기 때문에, " " 를 사용함..
     }
 }
