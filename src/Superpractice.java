39eece6bf86f4d402abf863a20e74a34223d7b52
IT-HONGREAT <hongreat95@gmail.com>
2021-10-06 23:55:30 +0900

    상속의 순서 확인을 위한 테스트코드

--- src/Superpractice.java
+++ src/Superpractice.java
@@ -1,15 +1,19 @@
 class SuperClass {
         SuperClass(){
             System.out.print("Super");
+            System.out.println("test3");
         }
 }
 class SubClass extends SuperClass{
     SubClass(){
         System.out.print("Sub");
+        System.out.println("test");
+        System.out.println("test2");
     }
 }
 class SuperSub{
     public static void main(String[] args){
         SuperClass s = new SubClass();
+        SuperClass n = new SubClass();
     }
 }
