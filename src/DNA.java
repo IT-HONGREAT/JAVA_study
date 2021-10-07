96cf9671048db9628e96d42cfae6f8f5893d05ae
IT-HONGREAT <hongreat95@gmail.com>
2021-10-07 23:59:51 +0900

    2중 반복문과 문자열 배열의 개념을 적립

---
+++ src/DNA.java
@@ -0,0 +1,61 @@
+public class DNA {
+    public static void main(String[] args){
+        String dna = "GATCCGCCCGCCTCGGCCTCCCAAAGTGCTGGGATTACAGGTGTGAGCCA"
+                + "CCACGCCCGGCTAATTTTTATTTATTTATTTAAAGACAGAGTCTCACTCT"
+                + "GTCACTCAGGCTAGAGTGCAGTGGCACCATCTCAGCTCACTGCAGCCTTG"
+                + "ACCTCCCTGGGCTCCGGTGATTTCACCCTCCCAAGTAGCTAGGACTACAG"
+                + "GCACATGCCACGACACCCAGCTAATTTTTTATTTTCTGTGAAGTCAAGGT"
+                + "CTTGCTACGTTGCCCATGCTGGTATCAAACCCCTGGGCTCAATCAATCCT"
+                + "TCCACCTCAGCCTCCCCAAGTATTGGGGTTACAGGCATGAGCTACCACAC"
+                + "TCAGCCCTAGCCTACTTGAAACGTGTTCAGAGCATTTAAGTTACCCTACA"
+                + "GTTGGGCAAAGTCATCTAACACAAAGCCCTTTTTATAGTAATAAAATGTT"
+                + "GTATATCTCATGTGATTTATTGAATATTGTTACTGAAAGTGAGAAACAGC"
+                + "ATGGTTGCATGAAAGGAGGCACAGTCGAGCCAGGCACAGCCTGGGCGCAG"
+                + "AGCGAGACTCAAAAAAAGAAAAGGCCAGGCGCACTGGCTCACGCCTGTAA"
+                + "TCCCAGCATTTCGGGAGGCTGAGGCGGGTGGATCACCTGAGGTCAGGAGT"
+                + "TCAAGACCAGCCTAGCCAACATGGTGAAACCCCGTCTCTACTAAAATACA"
+                + "AAAATTAACCGGGCGTGATGGCAGGTGCCTGTAATCCCAGCTACTTGGGA"
+                + "GGCTGAGGCAGGAGAATCGCTTGAACCAGGAGGCGGAGGTTGCAGGGAGC"
+                + "CAAGATGGCGCCACTGCACTCCAGCCTGGGCGATAGAGTGAGACTCCGTC"
+                + "TCAGAAAAAAAAGAAAAGAAACGAGGCACAGTCGCATGCACATGTAGTCC"
+                + "CAGTTACTTGAGAGGCTAAGGCAGGAGGATCTCTTGAGCCCAAGAGTTTG"
+                + "AGTCCAGCCTGAACAACATAGCAAGACATCATCTCTAAAATTTAAAAAAG"
+                + "GGCCGGGCACAGTGGCTCACACCTGTAATCCCAGCACTTTGGGAGGTGGA"
+                + "GGTGGGTAGATCACCTGACGTCAGGAGTTGGAAACCAGCCTGGCTAACAT";
+        char[] charArray = dna.toCharArray();   //문자열을 배열로
+
+        // TAGG, CCAG, AGCC
+        int tagg = 0, ccag = 0, agcc = 0;
+
+        char[] current = new char[4];
+        for (int i = 0; i < charArray.length - 3; i++) {
+            for (int j = 0; j < current.length ; j++) {
+                current[j] = charArray[i + j];
+            }
+            System.out.println(current);
+            String word = new String(current);  //배열을 문자열로
+
+            switch (word){
+                case "TAGG":
+                    tagg++;
+                    break;
+                case "CCAG":
+                    ccag++;
+                    break;
+                case "AGCC":
+                    agcc++;
+                    break;
+
+            }
+
+
+        }
+
+//         테스트
+        System.out.println("TAGG: " + tagg);
+        System.out.println("CCAG: " + ccag);
+        System.out.println("AGCC: " + agcc);
+
+
+    }
+}
